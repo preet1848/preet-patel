e4ae6a072107c72fcfc15ef6a71b89968340c92d
preet-patel <preetpatelrw@gmail.com>
2025-06-11 12:06:42 +0530

    preet

--- l6.py
+++ l6.py
@@ -22,7 +22,15 @@
       # i+=1 [ i = 2 + 1 ] = 3
       # i+=1 [ i = 3 + 1 ] = 4
       # i+=1 [ i = 4 + 1 ] = 5
-i = 0 
-while i <= 10:
-  print("while loop:" , i)
-  i+=1
+# i = 0 
+# while i <= 10:
+#   print("while loop:" , i)
+#   i+=1
+
+
+# nested loop
+
+for i in range(1 , 5):
+   for j in range(1 , 4):
+      print(j)
+      print()  
---
+++ l8.py
@@ -0,0 +1,33 @@
+# pattern in python
+
+# n = 7 
+# i = 0 
+# for i in range(n):
+    
+#     for j in range(i):
+#         print('*' ,  end=" ")
+#     print()
+
+
+# python number pattern 
+
+# for i in range(6):
+#     for j in range(1 , i + 1):
+#         print(j , end=" ")
+#     print()
+
+
+# string 
+
+# str1 = "Hello" + "world"
+# str2 = "Heloo" * 10
+# print(str2)
+
+# diamond pattern 
+
+for i in range(5):
+    print(' ' * (5 - i - 1) + '* ' * (i + 1))
+
+
+for i in range(5 - 2 , -1 , -1):
+    print(' ' * (5 - i - 1) + '* ' * (i + 1))
