1e7aa44a52dfb99480829e4eef79ab775d8d12a4
preet-patel <preetpatelrw@gmail.com>
2025-06-11 12:31:13 +0530

    preet

---
+++ l9.py
@@ -0,0 +1,41 @@
+# string formating 
+
+# string value using f charactor
+
+# name = "jhon"
+# age = 30
+
+# profile = "my name is {} and my age {}".format(name , age)
+
+# print(profile)
+# print("my name is {} and my age is {}".format(name  , age))
+
+
+
+# string with indexing and slicing 
+
+# statement = "hello  python!"
+# print(statement[-2])
+# print(statement[0])
+# print(statement[1])
+# print(statement[2])
+# print(statement[3])
+# print(statement[4])
+# print(statement[5])
+
+
+# print(statement[2:6])
+# print(statement[2:8])
+# print(statement[-5:-1])
+# print(statement[-1:-5])
+# print(statement[1:-5])
+
+# list 
+
+# list = [10 , 20 , 30]
+list = [10 , 20 , "hello"]
+
+print(list)
+print(list[0])
+print(list[1])
+print(list[2])
